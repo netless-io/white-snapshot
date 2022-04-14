2d4e62386aa976c1ae2efe4361f4c67cd1e8a4e0
hyrious <hyrious@outlook.com>
2022-04-14 10:28:14 +0800

    fix typo

--- test.ts
+++ test.ts
@@ -2,14 +2,14 @@ navigator.serviceWorker.getRegistrations().then((rs) => {
   rs.forEach((r) => r.unregister());
 });
 
-var src_orginal =
+var src_original =
   "https://flat-storage.oss-accelerate.aliyuncs.com/cloud-storage/2022-02/21/59455e11-d5fe-4f88-9d50-39b824a5c94b/59455e11-d5fe-4f88-9d50-39b824a5c94b.jpeg";
 var src_cors = "http://localhost:4000/img.jpeg";
 
-var chosen = src_cors;
+var chosen = location.search.includes('o') ? src_original : src_cors;
 
-var canvas = document.querySelector("canvas");
-var context = canvas.getContext("2d");
+var canvas = document.querySelector("canvas")!;
+var context = canvas.getContext("2d")!;
 
 var image = new Image();
 window.image = image;
@@ -23,7 +23,7 @@ image.onload = () => {
   // a.dispatchEvent(new MouseEvent("click"));
 };
 
-// If there's no CORS header (src_orginal)
+// If there's no CORS header (src_original)
 // 1. without this prop, the image can be drawn, but cannot be exported (toDataURL)
 // 2. with this prop, the image cannot be drawn, and cannot be exported
 
